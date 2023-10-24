3b5d7be913324070d1e6c3dcd36d2bd57dad4f2b
simon <simon.hobbs@electrooptical.net>
2023-10-24 01:57:40 -0400

    renaming, adding entry pint

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-requirements = []
-with open("requirements.txt", 'r') as f:
-    for line in f:
-        requirements.append(line)
+try:
+    requirements = []
+    with open("requirements.txt", 'r') as f:
+        for line in f:
+            requirements.append(line)
+except FileNotFoundError:
+    requirements = [
+        "click",
+        "pandas",
+        "jinja2"
+    ]
+
 
 description = ""
-with open("README.md", 'r') as f:
-    description = f.read()
+try:
+    with open("README.md", 'r') as f:
+        description = f.read()
+except FileNotFoundError:
+    pass
 
-setup(name='kicad-parts_placer',
+
+
+setup(name='excel_2_jinja2',
       version='0.1.0',
       long_description=description,
       long_description_content_type="text/markdown",
-      url='https://github.com/snhobbs/kicad-parts-placer.git',
+      url='https://github.com/snhobbs/render-df.git',
       author='Simon Hobbs',
       author_email='simon.hobbs@electrooptical.net',
       license='MIT',
@@ -30,7 +43,7 @@ setup(name='kicad-parts_placer',
       tests_require=['nose'],
       entry_points={
         "console_scripts": [
-            "kicad-parts-placer=kicad_parts_placer:main",
+            "excel-2-jinja2=excel_2_jinja2:cli_main",
         ],
       },
       include_package_data=False,
